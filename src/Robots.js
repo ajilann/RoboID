a52c6949a4856da3a698e532e0e41cee19642daf
ajilann <ajilan.nazarudeen@gmail.com>
2020-09-18 15:37:33 +0530

    added cards and robots component

---
+++ src/Robots.js
@@ -0,0 +1,58 @@
+export const Robots = [
+    {
+        id: 0,
+        name: 'James rathor',
+        username: 'Jarath',
+        email: 'jamesrathor@parta.com'
+
+    },
+    {
+        id: 1,
+        name: 'William Rector',
+        username: 'Willirec',
+        email: 'Willirec@gathi.com'
+
+    },
+    {
+        id: 2,
+        name: 'George West',
+        username: 'Geowe',
+        email: 'Georgewe@hippo.com'
+
+    },
+    {
+        id: 3,
+        name: 'Roy Kuriyan',
+        username: 'Rokur',
+        email: 'Roykuri@rating.com'
+
+    },
+    {
+        id: 4,
+        name: 'Angel Trim',
+        username: 'Angtri',
+        email: 'angeltri@hotmail.com'
+
+    },
+    {
+        id: 5,
+        name: 'Jabar tom',
+        username: 'Jabato',
+        email: 'jabatom@yahoo.com'
+
+    },
+    {
+        id: 6,
+        name: 'Isac Ibrahim',
+        username: 'Isabr',
+        email: 'isacibrahim@gmail.com'
+
+    },
+    {
+        id: 7,
+        name: 'Jesti Ebraham',
+        username: 'Jesebra',
+        email: 'Jestiebraham@mail.com'
+
+    }
+]
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Hello from './Hello';
+import Card from './Card';
+import {Robots} from './Robots'
 import 'tachyons'
 
 ReactDOM.render(
   <React.StrictMode>
-   <Hello greeting={'Hello ' + 'React Ninja'}/>
+   <Card id={Robots[0].id}name={Robots[0].name} username={Robots[0].username} email={Robots[0].email} />
+   <Card id={Robots[1].id}name={Robots[1].name} username={Robots[1].username} email={Robots[1].email} />
+   <Card id={Robots[2].id}name={Robots[2].name} username={Robots[2].username} email={Robots[2].email} />
   </React.StrictMode>,
   document.getElementById('root')
 );
